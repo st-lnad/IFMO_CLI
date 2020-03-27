90942b5870085eddafd6b7026ab0ad17d2e5ead9
st-lnad <46500729+st-lnad@users.noreply.github.com>
2020-03-27 19:07:31 +0300

    Add files via upload

---
+++ src_labs/Lab2_Hit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1.src_labs.Lab2
+{
+	static class Branching_2_2
+	{
+		private const short need_args = 3;
+		public static bool Result(double x, double y, double r)
+		{
+			if ((x <= 0) && (x >= -r) && (y >= 0))
+				return (((x + r) * (x + r) + y * y) <= r * r);
+			else if ((x >= 0) && (x <= r) && (y <= 0))
+				return (((x - r) * (x - r) + y * y) <= r * r);
+			else
+				return false;
+		}
+		public static void Run()
+		{
+			string[] args;
+			do
+			{
+				Console.Write("args = ");
+				args = Console.ReadLine().Split(' ');
+				if ((args.Length != need_args) ||
+					!double.TryParse(args[0], out double x) ||
+					!double.TryParse(args[1], out double y) ||
+					!double.TryParse(args[1], out double r))
+				{
+					if (!(args.Length == 1 && args[0] == "back")) PrintHelp();
+				}
+				else
+				{
+					Console.Write("result = ");
+					Console.WriteLine(Result(x, y, r));
+				}
+			} while (!(args.Length == 1 && args[0] == "back"));
+		}
+		private static void PrintHelp()
+		{
+			Console.WriteLine("This function calculates:");
+			Console.WriteLine("is your point a hit point");
+			Console.WriteLine("The syntax for this command is");
+			Console.WriteLine("x y r\n");
+		}
+	}
+}
---
+++ src_labs/Lab5_algorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1.src_labs.Lab5
+{
+	class Complex
+	{
+		private double Real, Imaginary;
+		public double getReal()
+		{
+			return Real;
+		}
+		public double getImaginary()
+		{
+			return Imaginary;
+		}
+
+		public Complex(double Re = 0, double Im = 0)
+		{
+			Real = Re;
+			Imaginary = Im;
+		}
+		
+		public Complex Computing(Complex a, Complex b) {
+			return new Complex(a.Real + b.Real, a.Imaginary + b.Imaginary);
+		}
+		public Complex Difference(Complex a, Complex b) {
+			return new Complex(a.Real - b.Real, a.Imaginary - b.Imaginary);
+		}
+		public Complex Product(Complex a, Complex b) {
+			return new Complex(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+		}
+		public double Module(Complex a) {
+			return Math.Sqrt(a.Real * a.Real + a.Imaginary * a.Imaginary);
+		}
+		public override string ToString() {
+			return Real.ToString() + " + " + Imaginary.ToString() + "i";
+		}
+
+		public static Complex operator +(Complex a, Complex b) => new Complex(a.Real + b.Real, a.Imaginary + b.Imaginary);
+		public static Complex operator -(Complex a, Complex b) => new Complex(a.Real - b.Real, a.Imaginary - b.Imaginary);
+		public static Complex operator *(Complex a, Complex b) => new Complex(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + a.Imaginary * b.Real);
+
+	}
+}
