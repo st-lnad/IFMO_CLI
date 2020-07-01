3efb396cb6bc096f33cf213d726f235e8f06b1c9
st-lnad <46500729+st-lnad@users.noreply.github.com>
2020-07-01 10:16:03 +0300

    Update Task7.cs

--- src_labs/Lab1/Task7.cs
+++ src_labs/Lab1/Task7.cs
@@ -1,12 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CLI_labs123.src.Lab1
+namespace Lev_labs.Lab1.Task7
 {
-	class Task7
+	static class Task7
 	{
+		internal static void Run()
+		{
+			try
+			{
+				Complex n = new Complex(0.0 / 0.0);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			Complex a = new Complex(15, 18);
+			Complex b = new Complex();
+			Complex c = a + b;
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);
+			Console.WriteLine(a.getReal() + b.getImaginary() * c.Module());
+		}
 	}
 }
