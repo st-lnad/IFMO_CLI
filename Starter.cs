6483c2addccb070799fbcff7419a8d5578416af9
st-lnad <46500729+st-lnad@users.noreply.github.com>
2020-07-01 10:03:52 +0300

    Update Starter.cs

--- Starter.cs
+++ Starter.cs
@@ -1,66 +1,63 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
 
-namespace ProjectProgram
+// Potanin Lev
+// Var 8
+
+namespace Lev_labs.Lab1.Starter
 {
-	class Starter
-	{
-		static Dictionary<string, Func<bool>> _labActions;
-
-		static void Main(string[] args)
+	static class Starter {
+		internal delegate void Runner();
+		internal static  Dictionary<string, Runner> actions = new Dictionary<string, Runner>
 		{
-			string path = "\nLP\\Main>";
-			Console.WriteLine("   Hello!\n   Write help:)");
-			do
+			{ "1", Task1.Run},
+			{ "2", Task2_1.Run},
+			{ "3", Task2_2.Run},
+			{ "4", Task3_1.Run},
+			{ "5", Task3_2.Run},
+			{ "6", Task4_1.Run},
+			{ "7", Task4_2.Run},
+			{ "8", Task5.Task5.Run},
+			//{ "9", Task6.Run},
+			{ "10", Task7.Task7.Run},
+			//{ "11", Task8.Run},
+		};
+		static string commands = "1 - Task 1 Linear programs\n" +
+			"2 - Task 2.1 Piecewise-defined function\n" +
+			"3 - Task 2.2 Hit point\n" +
+			"4 - Task 3.1 Table of function values\n" +
+			"5 - Task 3.2 Taylor series\n" +
+			"6 - Task 4.1 of rank 1\n" +
+			"7 - Task 4.2 of rank 2\n" +
+			"8 - Task 5 Strings\n" +
+			"9 - Task 6 Simple data structures\n" +
+			"10 - Task 7 Simple classes\n" +
+			"11 - Task 8  Standard collections\n";
+			
+		static void PrintActions()
+		{
+			Console.WriteLine(commands + "Now are available:");
+			foreach (var now in actions)
 			{
-				Console.Write(path);
-			} while (PerformCommand(Console.ReadLine().Split(' ')[0]));
-		}
-
-		public Starter()
-		{
-			_labActions = typeof(Starter).Assembly.GetTypes()
-										 .SelectMany(t => t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static))
-										 .Where(m => m.ReturnType == typeof(void) && m.GetParameters().Length == 0 && m.IsStatic)
-										 .Where(m => m.Name == "Run")
-										 .Select(m => CreateDelegate<Action>(m))
-										 .ToDictionary(
-												m => m.Method.DeclaringType.Name,
-												m => new Func<bool>(() => { m(); return true; }),
-												StringComparer.InvariantCultureIgnoreCase
-										  );
-
-			_labActions.Add("exit", () => false);
-		}
-
-		static T CreateDelegate<T>(MethodInfo method)
-		{
-			if (!typeof(Delegate).IsAssignableFrom(typeof(T)))
-				throw new ArgumentException("Type should be of delegate kind");
-			return (T)(object)Delegate.CreateDelegate(typeof(T), method);
-		}
-
-		private static bool PerformCommand(string com)
-		{
-			if (_labActions.TryGetValue(com, out var act))
-			{
-				return act();
+				Console.Write("{0} ", now.Key);
 			}
-			else
-			{
-				Console.WriteLine("Sorry, function is not implemented");
-				PrintHelp();
-				return true;
-			}
+			Console.WriteLine();
 		}
-		private static void PrintHelp()
+		public static void Main()
 		{
-			Console.WriteLine("List of available commands:");
-			foreach (var now in _labActions.Keys)
-			{
-				Console.WriteLine("  " + now);
+			string cmd = "";
+			while (cmd != "end") {
+				Console.WriteLine();
+				PrintActions();
+				Console.WriteLine();
+				cmd = Console.ReadLine().ToLower();
+				if (!actions.ContainsKey(cmd) && cmd != "end") { Console.WriteLine("Wrong key"); }
+				else actions[cmd]();
 			}
 		}
 	}
